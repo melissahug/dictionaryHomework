01833aba6d08326df8e3103e38c35b14d8e49b0f
Melissa <hughes.melissa1@gmail.com>
2016-09-20 15:46:12 -0400

    first commit

---
+++ DictionaryHomework/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> animalList = new Dictionary<string, int>();
+            animalList.Add("dog", 7);
+            animalList.Add("cat", 20);
+            animalList.Add("bird", 4);
+            animalList.Add("fish", 2);
+            animalList.Add("lamb", 15);
+
+           if (animalList.ContainsKey("dog"))
+            {
+                int value = animalList["dog"];
+                Console.WriteLine("Dog has " +value+ " legs");
+            }
+        } 
+    }
+}
